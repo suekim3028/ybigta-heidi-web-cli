633000f09b96d8b9f2de3f110eb3059cf55a9c8e
Suebin Kim <k.sue3028@gmail.com>
2024-03-04 03:09:56 +0900

    feat: add buttons in login page

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,25 +1,31 @@
 import { hasUserState } from "@atoms/user.atoms";
-import { Flex, Typography } from "antd";
+import { Button, Flex, Typography } from "antd";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import Login from "./components/Login/Login";
+import { userHooks } from "@hooks";
 
 const LoginPage = () => {
-  const hasUser = useRecoilValue(hasUserState);
   const router = useRouter();
-
-  useEffect(() => {
-    if (hasUser) {
-      router.replace("/");
-    }
-  }, [hasUser]);
-
+  userHooks.useGuestOnly();
   return (
     <>
       <Flex vertical>
         <Typography.Title>Login</Typography.Title>
         <Login />
+        <Flex vertical={false} gap={"small"}>
+          <Button
+            size="large"
+            type={"dashed"}
+            onClick={() => router.push("sign-up")}
+          >
+            회원가입
+          </Button>
+          <Button type="text" size="large">
+            아이디/비밀번호 찾기
+          </Button>
+        </Flex>
       </Flex>
     </>
   );
