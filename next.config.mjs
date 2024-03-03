07b024b63579162120f042c7130859ffda308e44
Suebin Kim <k.sue3028@gmail.com>
2024-03-03 15:59:38 +0900

    feat: set transpile config for antd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    "antd",
+    "rc-util",
+    "@babel/runtime",
+    "@ant-design/icons",
+    "@ant-design/icons-svg",
+    "rc-pagination",
+    "rc-picker",
+    "rc-tree",
+    "rc-table",
+    "kitchen-flow-editor",
+  ],
+  images: {
+    remotePatterns: [
+      // TODO: TEST
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
