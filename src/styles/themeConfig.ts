d24e6bfb7c7e6de7ffd55f1b8fdd6cd035771b96
Suebin Kim <k.sue3028@gmail.com>
2024-03-03 14:36:59 +0900

    feat: config antd

---
+++ src/styles/themeConfig.ts
@@ -0,0 +1,11 @@
+// theme/themeConfig.ts
+import type { ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#537A5A",
+  },
+};
+
+export default theme;
