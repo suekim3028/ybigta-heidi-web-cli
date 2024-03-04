50d5271f9b9f5e1453b7e7a0b9ac630374936b37
Suebin Kim <k.sue3028@gmail.com>
2024-03-04 17:53:17 +0900

    feat: add level

--- src/pages/index/components/UserProfile/UserProfile.tsx
+++ src/pages/index/components/UserProfile/UserProfile.tsx
@@ -1,6 +1,15 @@
 import { STYLE_CONSTS } from "@constants";
 import { useSignedInUserOnly } from "@contexts";
-import { Avatar, Button, Col, Flex, Statistic, Tag, Typography } from "antd";
+import {
+  Avatar,
+  Button,
+  Card,
+  Col,
+  Flex,
+  Statistic,
+  Tag,
+  Typography,
+} from "antd";
 
 const { Text, Link } = Typography;
 
@@ -27,14 +36,34 @@ const UserProfile = () => {
           <Button size={"middle"}>개인정보 수정</Button>
         </Flex>
       </Flex>
-      <Flex>
+      <Flex
+        vertical
+        style={{
+          backgroundColor: "rgba(0,0,0,0.1)",
+          padding: 20,
+          borderRadius: 20,
+        }}
+        gap={20}
+      >
+        <Flex flex={1} gap={12}>
+          <Text strong>건강 레벨</Text>
+          <Tag color={"magenta"}>level. 4</Tag>
+        </Flex>
         <Flex flex={1} gap={28}>
           <Col span={100}>
-            <Statistic title="누적한 건강 효과" value={1123} suffix={"kcal"} />
+            <Card bordered={false}>
+              <Statistic
+                title="누적한 건강 효과"
+                value={1123}
+                suffix={"kcal"}
+              />
+            </Card>
           </Col>
 
           <Col span={100}>
-            <Statistic title="누적 경제적 효과" value={324} suffix={"만원"} />
+            <Card bordered={false}>
+              <Statistic title="누적 경제적 효과" value={324} suffix={"만원"} />
+            </Card>
           </Col>
         </Flex>
       </Flex>
