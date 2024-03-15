2bedb108cf90d52af8cb25b566a4465551fd2523
Suebin Kim <k.sue3028@gmail.com>
2024-03-16 01:42:41 +0900

    feat: set banner images

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,7 +3,11 @@ import { Image } from "antd";
 
 const Banner = ({ imageUrl, link }: CommonTypes.Banner) => {
   return (
-    <Image src={imageUrl} style={{ objectFit: "cover" }} preview={false} />
+    <Image
+      src={imageUrl}
+      style={{ objectFit: "contain", objectPosition: "center" }}
+      preview={false}
+    />
   );
 };
 
--- src/components/BannerCarousel/BannerCarousel.tsx
+++ src/components/BannerCarousel/BannerCarousel.tsx
@@ -4,14 +4,13 @@ import Banner from "@components/Banner/Banner";
 import { CommonTypes } from "@types";
 
 const BannerCarousel = () => {
-  const banners: CommonTypes.Banner[] = Array.from({ length: 4 }, (_, idx) => ({
-    imageUrl:
-      "https://www.adobe.com/kr/creativecloud/photography/hub/features/media_1b08f0078230faac6814da00eb20b5f98e9aff78d.png?width=1200&format=pjpg&optimize=medium",
-    link: "www.naver.com",
+  const banners: CommonTypes.Banner[] = Array.from({ length: 4 }, (_, i) => ({
+    imageUrl: `/images/banner${i + 1}.png`,
+    link: "",
   }));
 
   return (
-    <Carousel autoplay={false} style={{ height: "calc(100vw*0.4)" }}>
+    <Carousel autoplay style={{ height: "calc(100vw*0.4)" }}>
       {banners.map((banner) => (
         <Banner {...banner} />
       ))}
