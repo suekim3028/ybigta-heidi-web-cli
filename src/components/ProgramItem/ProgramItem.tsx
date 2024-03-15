ca3a8169a68d7acf309a6fe8a7e5a835461ddcd4
Suebin Kim <k.sue3028@gmail.com>
2024-03-15 22:07:47 +0900

    feat: add place in program item

--- src/components/ProgramItem/ProgramItem.tsx
+++ src/components/ProgramItem/ProgramItem.tsx
@@ -6,25 +6,33 @@ import { useRouter } from "next/router";
 import { programUtils } from "@utils";
 const { Text } = Typography;
 
-const ProgramItem = ({ name, rate, category, id }: ProgramTypes.Mini) => {
+const ProgramItem = ({
+  name,
+  rate,
+  category,
+  id,
+  place,
+}: ProgramTypes.Mini) => {
   const router = useRouter();
 
   return (
     <Flex
       justify="space-between"
       style={{ padding: 10 }}
-      align="center"
+      align="flex-start"
       onClick={() => router.push(`/program/${id}`)}
     >
-      <Flex align={"center"} gap={12}>
-        <Image
-          src={programUtils.getProgramThumbnail({ category })}
-          preview={false}
-          width={60}
-          height={60}
-          style={{ borderRadius: 10 }}
-        />
-        <Flex vertical align={"flex-start"} gap={2}>
+      <Flex align={"center"} gap={12} justify="space-between" flex={1}>
+        <Flex flex={0}>
+          <Image
+            src={programUtils.getProgramThumbnail({ category })}
+            preview={false}
+            width={60}
+            height={60}
+            style={{ borderRadius: 10, display: "flex" }}
+          />
+        </Flex>
+        <Flex vertical align={"flex-start"} flex={1}>
           <Tag
             color={PROGRAM_CONTS.CATEGORY_TAG_COLOR[category]}
             style={{ fontSize: 12, padding: "0px 4px" }}
@@ -35,11 +43,22 @@ const ProgramItem = ({ name, rate, category, id }: ProgramTypes.Mini) => {
           <Text style={{ fontSize: 16 }} strong>
             {name}
           </Text>
+          <Text style={{ fontSize: 12, color: "rgba(0,0,0,0.4)" }}>
+            {place}
+          </Text>
         </Flex>
-      </Flex>
-      <Flex style={{ color: "orange" }} gap={2} align="center">
-        <StarOutlined size={10} />
-        <Text style={{ color: "orange", fontSize: 14 }}>{rate.toFixed(1)}</Text>
+
+        <Flex
+          style={{ color: "orange", justifySelf: "flex-end" }}
+          gap={2}
+          align="center"
+          flex={0}
+        >
+          <StarOutlined size={10} />
+          <Text style={{ color: "orange", fontSize: 14 }}>
+            {rate.toFixed(1)}
+          </Text>
+        </Flex>
       </Flex>
     </Flex>
   );
